1a6a4e3320b31a5eb2d04deeb2034eef6db84c3b
Kwon-DoHee <152317074+seamooll@users.noreply.github.com>
2025-11-04 05:31:38 +0900

    feat: 6주차 미션 완료

---
+++ 라미/server/src/main/java/umc/server/domain/review/controller/ReviewController.java
@@ -0,0 +1,29 @@
+package umc.server.domain.review.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import umc.server.domain.review.entity.Review;
+import umc.server.domain.review.service.ReviewQueryService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class ReviewController {
+
+    private final ReviewQueryService reviewQueryService;
+
+    public ReviewController(ReviewQueryService reviewQueryService) {
+        this.reviewQueryService = reviewQueryService;
+    }
+
+    @GetMapping("/reviews/search")
+    public List<Review> searchReview(
+            @RequestParam String query,
+            @RequestParam String type
+    ){
+        List<Review> result = reviewQueryService.searchReview(query, type);
+        return result;
+    }
+}
--- 라미/server/src/main/java/umc/server/domain/review/repository/ReviewRepository.java
+++ 라미/server/src/main/java/umc/server/domain/review/repository/ReviewRepository.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 import umc.server.domain.review.entity.Review;
 
 @Repository
-public interface ReviewRepository extends JpaRepository<Review, Long> {
+public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewQueryDsl {
 
     // 리뷰 작성 쿼리(insert)는 save() 메서드가 이미 JpaRepository에 구현되어 있으므로
     // 따로 메서드 작성하지 않았습니다.
