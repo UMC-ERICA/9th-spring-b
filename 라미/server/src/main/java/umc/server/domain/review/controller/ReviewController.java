df115b578496743c41e430469b83431f172b0978
Kwon-DoHee <152317074+seamooll@users.noreply.github.com>
2025-11-04 14:25:30 +0900

    fix: 수동 생성자를 Lombok 어노테이션으로 대체

--- 라미/server/src/main/java/umc/server/domain/review/controller/ReviewController.java
+++ 라미/server/src/main/java/umc/server/domain/review/controller/ReviewController.java
@@ -1,22 +1,19 @@
 package umc.server.domain.review.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import umc.server.domain.review.entity.Review;
 import umc.server.domain.review.service.ReviewQueryService;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class ReviewController {
 
     private final ReviewQueryService reviewQueryService;
-
-    public ReviewController(ReviewQueryService reviewQueryService) {
-        this.reviewQueryService = reviewQueryService;
-    }
 
     @GetMapping("/reviews/search")
     public List<Review> searchReview(
