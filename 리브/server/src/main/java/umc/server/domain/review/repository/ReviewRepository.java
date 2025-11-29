ba843571d625a84a44f74ef1dbeb9d124b26e5fe
leesiyeon <sylee3182@gmail.com>
2025-11-29 20:21:50 +0900

    refactor: ReviewRepository 파라미터를 PageRequest에서 Pageable로 변경

--- 리브/server/src/main/java/umc/server/domain/review/repository/ReviewRepository.java
+++ 리브/server/src/main/java/umc/server/domain/review/repository/ReviewRepository.java
@@ -1,22 +1,16 @@
 package umc.server.domain.review.repository;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import umc.server.domain.member.enums.FoodType;
 import umc.server.domain.review.entity.Review;
 import umc.server.domain.store.entity.Store;
-
-import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewQueryDsl {
 
     Page<Review> findAllByOrderByCreatedAtDesc(Pageable pageable);
 
-    Page<Review> findAllByStore(Store store, PageRequest pageRequest);
+    Page<Review> findAllByStore(Store store, Pageable pageRequest);
 
     Page<Review> findAllByMemberId(Long memberId, Pageable pageable);
 }
